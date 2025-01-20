1fd9b20e2805c14a4934f14b9b669c97e00ee713
Haniel Oliveira <hanielpedreira@gmail.com>
2025-01-19 21:46:50 -0300

    Inserção de alguns comentários e atualização do readme

--- Tarefa-2.c
+++ Tarefa-2.c
@@ -12,6 +12,7 @@
 // Função para desligar todos os LEDs
 void turn_off_leds()
 {
+    // Coloca os pinos dos LEDs em estado baixo (desligado)
     gpio_put(LED_RED, 0);
     gpio_put(LED_GREEN, 0);
     gpio_put(LED_BLUE, 0);
@@ -44,22 +45,23 @@ void setup_gpio()
 // Função para emitir som no buzzer
 void buzz_sound()
 {
-    // Gera um som por 500 ms
+    // Gera um som pulsando o buzzer por 500 ms
     printf("Emitindo som no buzzer.\n");
     for (int i = 0; i < 500; i++)
     {
-        gpio_put(BUZZER, 1);
-        sleep_us(1000); // 1 ms ligado
-        gpio_put(BUZZER, 0);
-        sleep_us(1000); // 1 ms desligado
+        gpio_put(BUZZER, 1); // Liga o buzzer
+        sleep_us(1000);      // Aguarda 1 ms
+        gpio_put(BUZZER, 0); // Desliga o buzzer
+        sleep_us(1000);      // Aguarda 1 ms
     }
 }
 
-// Função para processar comandos
+// Função para processar comandos recebidos
 void process_command(char command_)
 {
     if (command_ == 'r')
     {
+        // Liga o LED vermelho e apaga os outros
         gpio_put(LED_RED, 1);
         printf("LED vermelho ligado.\n");
 
@@ -68,6 +70,7 @@ void process_command(char command_)
     }
     else if (command_ == 'g')
     {
+        // Liga o LED verde e apaga os outros
         gpio_put(LED_GREEN, 1);
         printf("LED verde ligado.\n");
 
@@ -76,6 +79,7 @@ void process_command(char command_)
     }
     else if (command_ == 'b')
     {
+        // Liga o LED azul e apaga os outros
         gpio_put(LED_BLUE, 1);
         printf("LED azul ligado.\n");
 
@@ -84,15 +88,18 @@ void process_command(char command_)
     }
     else if (command_ == '0')
     {
+        // Apaga todos os LEDs
         printf("LEDS APAGADOS.\n");
         turn_off_leds();
     }
     else if (command_ == 's')
     {
-        buzz_sound(); // Emite som no buzzer
+        // Emite som no buzzer
+        buzz_sound();
     }
     else
     {
+        // Comando inválido
         printf("Comando desconhecido: %c\n", command_);
     }
 }
@@ -101,7 +108,7 @@ int main()
 {
     // Inicializar a comunicação serial e configurar GPIOs
     stdio_init_all(); // Inicializa a comunicação serial com baud rate 115200
-    setup_gpio();
+    setup_gpio();     // Configura os GPIOs
 
     char command;
 
@@ -109,8 +116,9 @@ int main()
     {
         if (stdio_usb_connected())
         {
+            // Lê um caractere da entrada serial
             command = getchar();      // Ler caractere
-            process_command(command); // Função que processa comando
+            process_command(command); // Processa o comando recebido
         }
     }
 
